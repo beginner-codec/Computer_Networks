e91e236c54b88f03497abc14bd55e02b8288e255
beginner-codec <nirupa.kulandaisamy@gmail.com>
2026-08-11 20:45:44 +0530

    Create numcomp.c

---
+++ numcomp.c
@@ -0,0 +1,10 @@
+//Leetcode 476. Number Complement
+int findComplement(int num) {
+    int mask = 0;
+    int temp = num;
+    while (temp > 0) {
+        mask = (mask << 1) | 1;
+        temp >>= 1;
+    }
+    return num ^ mask;
+}
